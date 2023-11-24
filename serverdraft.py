ed4a34155d09f4b5cbd071da6a0f092f90e2bdd6
brenteatworld <brent.monkhorst@gmail.com>
2023-11-24 18:22:56 +1100

    Update serverdraft.py

--- serverdraft.py
+++ serverdraft.py
@@ -11,3 +11,13 @@ server.bind(("0.0.0.0, 1989"))
 # limit number of connections before refusing new connections
 server.listens(5)
 
+# client keeps track of connected clients in a dictionary
+clients = {}
+# audit log keep a log of messages about user activity
+audit_log = []
+
+#handling of comms with connected client - run in separate thread for each connected client - username up to 1024 bytes logged and tracked in dictionary.
+def handle_client(client_socket):
+    user = client_socket.recv(1024).decode('utf-8')
+    audit_log.append(f"{user} came online.")
+
