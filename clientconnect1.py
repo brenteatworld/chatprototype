c950d4d9bbadc110acc0b57a48e2837bedc2e4bf
brenteatworld <brent.monkhorst@gmail.com>
2023-11-25 14:45:43 +1100

    test script for username comms to server

--- clientconnect1.py
+++ clientconnect1.py
@@ -7,3 +7,9 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('127.0.0.1', 1989) # server IP set to local host - port 1989 used in server script.
 client_socket.connect(server_address)
 
+# send hardcoded username - to be used in a later version of app
+client_socket.sendall('brent-client-test'.encode('utf-8'))
+print("Username sent to server!")
+
+# close connection
+client_socket.close()
