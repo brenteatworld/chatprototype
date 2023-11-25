8ba5f37dd9cb2991b10a1ddb8863366bcc49cf5a
brenteatworld <brent.monkhorst@gmail.com>
2023-11-25 16:11:18 +1100

    commit - test stay connected to server

--- clientconnect1.py
+++ clientconnect1.py
@@ -2,6 +2,19 @@
 
 import socket
 
+def receive_messages(sock):
+    """Handles receiving messages from the server."""
+    while True:
+        try:
+            message = sock.recv(1024).decoe('utf-8')
+            if message:
+                print(message)
+            else:
+                # empty message means server has closed the connection.
+                print("Disconnected from server!")
+                break
+            
+
 # client set up
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('127.0.0.1', 1989) # server IP set to local host - port 1989 used in server script.
