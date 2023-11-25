65b2ac253260f6bb46339c544c8e63bd66acdaef
brenteatworld <brent.monkhorst@gmail.com>
2023-11-25 13:49:10 +1100

    function 1 added

--- serverv1_master.py
+++ serverv1_master.py
@@ -10,3 +10,16 @@ server_address = ('0.0.0.0', 1989)
 server_socket.bind(server_address)
 server_socket.listen()
 
+# store active clients - username: socket
+clients = {}
+# maintain audit log of messages
+audit_log = []
+
+# function handling broadcasting of messages to all clients
+def broadcast_message(message):
+    for client_socket in clients.values():
+        try:
+            client_socket.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"Error sending message! {e}")
+
